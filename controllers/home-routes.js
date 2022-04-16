54b848a482ee891275e7fa96f5c3ae615207b4fc
ApolloSolo <matt1teixeira@gmail.com>
2022-04-16 18:06:32 -0400

    Render comments section based on session data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,7 +30,10 @@ router.get('/', (req, res) => {
       .then(dbPostData => {
           const posts = dbPostData.map(post => post.get({ plain: true }));
         // pass a single post object into the homepage template
-        res.render('homepage', { posts });
+        res.render('homepage', {
+          posts,
+          loggedIn: req.session.loggedIn
+        });
       })
       .catch(err => {
         console.log(err);
@@ -84,12 +87,21 @@ router.get('/', (req, res) => {
         const post = dbPostData.get({ plain: true });
   
         // pass data to template
-        res.render('single-post', { post });
+        res.render('single-post', { post, loggedIn: req.session.loggedIn });
       })
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
       });
+  });
+
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
   });
 
 module.exports = router;
