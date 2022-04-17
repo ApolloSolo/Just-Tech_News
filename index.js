efaba1a9fed6693e79b14471f4d4f07072afc5d9
ApolloSolo <matt1teixeira@gmail.com>
2022-04-17 10:29:44 -0400

    implemented partials and added own handelbar helpers

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers')
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -17,7 +18,7 @@ const sess = {
   store: new SequelizeStore({
     db: sequelize
   })
-}; //
+};
 
 app.use(session(sess));
 
